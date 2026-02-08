863d704607185693cdda526bc82721edc6ba720a
VovaVal <malinskiyvova@gmail.com>
2026-02-08 20:59:22 +0300

    Set the location for the window

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ from game_resorces import SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE, background_mu
 
 
 def main():
-    window = arcade.Window(width=SCREEN_WIDTH, height=SCREEN_HEIGHT, title=GAME_TITLE)
+    window = arcade.Window(width=SCREEN_WIDTH, height=SCREEN_HEIGHT, title=GAME_TITLE, visible=False)
+    screen_width, screen_height = arcade.get_display_size()
+    window.set_location(
+        (screen_width - SCREEN_WIDTH) // 2,
+        50
+    )
+    window.set_visible(True)
 
     background_music = arcade.load_sound(background_music_sound_path)
     player_music = arcade.play_sound(background_music, loop=True)
